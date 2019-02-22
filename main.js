c35d5e15a083bed6213834a1b26e1368c3806fca
stankovicjovana <47898918+stankovicjovana@users.noreply.github.com>
2019-02-22 19:16:44 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+	var mySrc = myImage.getAttribute('src');
+	
+	if(mySrc === 'images/bella4months.jpeg') {
+		myImage.setAttribute('src', 'images/bella2months.jpg');
+	} else {
+		myImage.setAttribute('src', 'images/bella4months.jpeg');
+	}
+
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+	var myName = prompt('Please enter your name.');
+	localStorage.setItem('name', myName);
+	myHeading.textContent = myName + ', Meet my dog!';
+}
+
+if(!localStorage.getItem('name')) {
+	setUserName();
+} else {
+	var storedName = localStorage.getItem('name');
+	myHeading.textContent = storedName + ', Meet my dog!';
+}
+
+myButton.onclick = function() {
+	setUserName();
+}
